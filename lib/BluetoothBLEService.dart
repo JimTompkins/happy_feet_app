32b1b62fa960dbb516f4ad8f5275e1a1cb9dcf81
Jim Tompkins <jim.tompkins@gmail.com>
2021-04-26 14:33:17 -0300

    Added methods enableBeat, disableBeat, processBeats

--- lib/BluetoothBLEService.dart
+++ lib/BluetoothBLEService.dart
@@ -55,7 +55,7 @@ class BluetoothBLEService {
   BluetoothCharacteristic? _char6;
 
   StreamSubscription<List<int>>? _dataReadCharacteristicSubscription;
-  StreamSubscription<List<int>>? _char4Subscription;
+  StreamSubscription<List<int>>? _beatSubscription;
 
   String? connectionText = "";
   List<BluetoothDevice>? _devicesList;
@@ -82,8 +82,8 @@ class BluetoothBLEService {
   final _dataReceivedSubject = BehaviorSubject<List<int>>();
   Stream<List<int>> get dataReceivedStream => _dataReceivedSubject.stream;
 
-  final _char4Subject = BehaviorSubject<List<int>>();
-  Stream<List<int>> get char4Stream => _char4Subject.stream;
+  final _beatSubject = BehaviorSubject<List<int>>();
+  Stream<List<int>> get beatStream => _beatSubject.stream;
 
   isDeviceBluetoothOn() async {
     try {
@@ -263,34 +263,6 @@ class BluetoothBLEService {
     }
   }
 
-  // enable notifications on char4
-  enableBeatNotifications() async {
-    if (_char4 == null) return;   // check for non-null char4
-    try {
-      if (_char4!.properties.notify) {
-        await _char4!.setNotifyValue(true);
-
-        _char4Subscription?.cancel();
-        _char4Subscription =
-            _char4!.value.listen((data) {
-              print("HF: _char4 data: ");
-              print(data);
-              print("last value:");
-              print(_char4!.lastValue);
-              if (data.length > 0) {
-                // this is to fix the bluetooth still remembers the last values from previous connection.
-                if (_char4!.lastValue != data) {
-                  _char4Subject.add(data);
-                }
-              }
-            });
-      }
-    } catch (err) {
-      print("HF:  error enabling notifications on char4 ");
-      print(err);
-    }
-  }
-
   // write a characteristic on HappyFeet.  Char 3 and 6 are
   // writable and single byte
   writeChar(List<int> bytes, int charNum) async {
@@ -325,6 +297,87 @@ class BluetoothBLEService {
         // shouldn't get here...
       }
       break;
+    }
+  }
+
+  // enable the beat notification by writing char6 to 0x01
+  enableBeat() async {
+    List<int> data = [0x01];
+    if (_char6 == null) return;
+    if (_char6!.properties.write) {
+      try {
+        await _char6!.write(data);
+        print("HF: enabled beat detection");
+      } catch (err) {
+        print("HF: error enable beat");
+        print(err);
+      }
+    }
+  }
+
+  // disable the beat notification by writing char6 to 0x00
+  disableBeat() async {
+    List<int> data = [0x00];
+    if (_char6 == null) return;
+    if (_char6!.properties.write) {
+      try {
+        await _char6!.write(data);
+        print("HF: disabled beat detection");
+      } catch (err) {
+        print("HF: error disableBeat");
+        print(err);
+      }
+    }
+  }
+
+  // read the accelerometer's whoAmI register reading from char2
+  // the value should be 0x44
+  readWhoAmI() async {
+    if (_char2 == null) return;
+    if (_char2!.properties.read) {
+      try {
+        List<int> value = await _char2!.read();
+        if (value[0] == 0x44) {
+          print("HF: correct whoAmI value was read");
+        } else {
+          print("HF: *** incorrect whoAmI value was read");
+        }
+      } catch (err) {
+        print("HF: error readWhoAmI");
+        print(err);
+      }
+    }
+  }
+
+  // method to process beats received as notifications on char4
+  processBeats() async {
+    if (_char4 == null) return;
+
+    print("HF: process beats");
+
+    try {
+      if (_char4!.properties.notify) {
+        await _char4!.setNotifyValue(true);
+
+        _beatSubscription?.cancel();
+        _beatSubscription =
+            _char4!.value.listen((data) {
+              print("Beat data received: ");
+              print(data);
+              print("last value:");
+              print(_char4!.lastValue);
+              // play the next note in the groove
+              if (data.length > 0) {
+                // this is to fix the bluetooth still remembers the last values from previous connection.
+                if (_char4!.lastValue != data) {
+                  _beatSubject.add(data);
+                }
+              }
+            });
+      }
+    } catch (err) {
+      print("HF: error _char4: ");
+      print(err);
     }
   }
 
